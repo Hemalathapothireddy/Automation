5e216904f367820641f601ae12655d4bfe32bd1c
Hemalathapothireddy <hemajayanthreddy@gmail.com>
2017-05-25 00:53:34 -0400

    Update GoogleHomePage.java
    test

--- MercuryPOM/src/com/pages/GoogleHomePage.java
+++ MercuryPOM/src/com/pages/GoogleHomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 //one page one class
+//this is be hema
 public class GoogleHomePage {
 	WebDriver driver;
 	public GoogleHomePage(WebDriver driver){
